e0aa519352a4a806111b11d706a1c5b0a4caa719
madhavi <madhavibhatt72@gmail.com>
2017-03-11 18:43:40 +0000

    Added role-based access control

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import connectDB from "./config/database.js";
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Last updated: 2025-02-20T20:40:07.382Z
