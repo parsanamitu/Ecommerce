9adc91858cc31d4497581d59f3db27ad37aada08
madhavi <madhavibhatt72@gmail.com>
2017-10-31 12:07:58 +0000

    Optimized DB queries for faster performance

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,3 +15,4 @@ export const getUsers = async (req, res) => {
 // Last updated: 2025-02-20T20:42:21.392Z
 // Last updated: 2025-02-20T20:42:23.262Z
 // Last updated: 2025-02-20T20:42:26.765Z
+// Last updated: 2025-02-20T20:42:27.244Z
