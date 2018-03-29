233e411345e1bf0149b875408ee8d90070d774d9
madhavi <madhavibhatt72@gmail.com>
2018-03-29 23:32:53 +0100

    Added role-based access control

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,3 +33,4 @@ export const getUsers = async (req, res) => {
 // Last updated: 2025-02-20T20:49:50.734Z
 // Last updated: 2025-02-20T20:49:51.183Z
 // Last updated: 2025-02-20T20:49:51.929Z
+// Last updated: 2025-02-20T20:49:53.238Z
