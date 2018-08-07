e39218ad52f2f43c64860c9a9ca7e676cec9ec4c
Mitwa <parsanamitu@gmail.com>
2018-08-07 13:03:37 +0100

    Implemented JWT authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -184,3 +184,4 @@ export const getUsers = async (req, res) => {
 // Last updated: 2025-02-20T22:15:26.802Z
 // Last updated: 2025-02-20T22:15:28.515Z
 // Last updated: 2025-02-20T22:15:30.545Z
+// Last updated: 2025-02-20T22:15:32.019Z
