98aa98a576011b7968501ce8d975b343c2d0c7e8
Mitwa <parsanamitu@gmail.com>
2018-08-28 13:32:56 +0100

    Implemented JWT authentication

--- models/User.js
+++ models/User.js
@@ -171,3 +171,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T22:15:14.054Z
 // Last updated: 2025-02-20T22:15:15.264Z
 // Last updated: 2025-02-20T22:15:15.755Z
+// Last updated: 2025-02-20T22:15:17.273Z
