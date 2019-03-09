bee7eeb9e0c240fe18b2e51e6921e13ee4606970
madhavi <madhavibhatt72@gmail.com>
2019-03-09 11:54:07 +0000

    Implemented JWT authentication

--- server.js
+++ server.js
@@ -32,3 +32,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Last updated: 2025-02-20T20:42:36.760Z
 // Last updated: 2025-02-20T20:42:38.990Z
 // Last updated: 2025-02-20T20:49:47.727Z
+// Last updated: 2025-02-20T20:49:48.189Z
