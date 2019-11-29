11ff5dbc30b444be1473a18bd980af4b162e6965
Mitwa <parsanamitu@gmail.com>
2019-11-29 12:48:15 +0000

    Fixed session expiration issue

--- controllers/userController.js
+++ controllers/userController.js
@@ -177,3 +177,4 @@ export const getUsers = async (req, res) => {
 // Last updated: 2025-02-20T22:15:13.192Z
 // Last updated: 2025-02-20T22:15:15.024Z
 // Last updated: 2025-02-20T22:15:16.532Z
+// Last updated: 2025-02-20T22:15:19.021Z
