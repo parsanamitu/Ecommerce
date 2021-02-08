ddba3f3f581392869362d7e1389d38082da40e03
madhavi <madhavibhatt72@gmail.com>
2021-02-08 17:19:13 +0000

    Refactored API routes for better structure

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+import User from "../models/User.js";
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+// Last updated: 2025-02-20T20:40:07.880Z
