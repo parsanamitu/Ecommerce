59bba2408874200094c22f832c0d40b8eb4a6b99
madhavi <madhavibhatt72@gmail.com>
2021-03-24 18:36:29 +0000

    Enhanced security by sanitizing user input

--- models/User.js
+++ models/User.js
@@ -101,3 +101,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T21:42:08.530Z
 // Last updated: 2025-02-20T21:42:09.273Z
 // Last updated: 2025-02-20T21:42:12.511Z
+// Last updated: 2025-02-20T21:42:14.068Z
