85f37be7bd57cdd4e0554e303e2302f894154a2e
madhavi <madhavibhatt72@gmail.com>
2021-05-04 20:05:48 +0100

    Improved logging for error tracking

--- models/User.js
+++ models/User.js
@@ -64,3 +64,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T20:58:28.614Z
 // Last updated: 2025-02-20T20:58:29.551Z
 // Last updated: 2025-02-20T20:58:34.128Z
+// Last updated: 2025-02-20T20:58:34.356Z
