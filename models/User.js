466b6d8133b94239951d80b92e3f0df8a7fd1a06
madhavi <madhavibhatt72@gmail.com>
2021-05-30 17:27:48 +0100

    Implemented WebSocket support

--- models/User.js
+++ models/User.js
@@ -97,3 +97,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T21:42:01.011Z
 // Last updated: 2025-02-20T21:42:03.338Z
 // Last updated: 2025-02-20T21:42:04.498Z
+// Last updated: 2025-02-20T21:42:05.430Z
