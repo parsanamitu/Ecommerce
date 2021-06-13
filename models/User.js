ad7d3cbd8b4d31de909a2a1b7828bc31a7b38e3a
madhavi <madhavibhatt72@gmail.com>
2021-06-13 18:34:27 +0100

    Added role-based access control

--- models/User.js
+++ models/User.js
@@ -69,3 +69,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T20:58:39.750Z
 // Last updated: 2025-02-20T20:58:41.934Z
 // Last updated: 2025-02-20T20:58:42.542Z
+// Last updated: 2025-02-20T20:58:43.082Z
