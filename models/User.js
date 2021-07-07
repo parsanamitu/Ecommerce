b271954f82dafd16a69da49e05fd878ef2bd97d8
madhavi <madhavibhatt72@gmail.com>
2021-07-07 20:59:10 +0100

    Refactored API routes for better structure

--- models/User.js
+++ models/User.js
@@ -86,3 +86,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T20:59:10.502Z
 // Last updated: 2025-02-20T20:59:13.978Z
 // Last updated: 2025-02-20T21:41:44.897Z
+// Last updated: 2025-02-20T21:41:45.126Z
