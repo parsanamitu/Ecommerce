80c7d59055efe0b897d2185c259ec99e18e93764
madhavi <madhavibhatt72@gmail.com>
2021-09-29 14:41:02 +0100

    Implemented WebSocket support

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,3 +64,4 @@ export const getUsers = async (req, res) => {
 // Last updated: 2025-02-20T20:58:38.492Z
 // Last updated: 2025-02-20T20:58:40.994Z
 // Last updated: 2025-02-20T20:58:43.957Z
+// Last updated: 2025-02-20T20:58:51.006Z
