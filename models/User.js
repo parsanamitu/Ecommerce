49c65a4d1dd50b04f0fa33b053895db1c3b61d35
madhavi <madhavibhatt72@gmail.com>
2021-09-30 23:50:04 +0100

    Optimized DB queries for faster performance

--- models/User.js
+++ models/User.js
@@ -60,3 +60,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T20:50:33.334Z
 // Last updated: 2025-02-20T20:50:35.385Z
 // Last updated: 2025-02-20T20:50:41.114Z
+// Last updated: 2025-02-20T20:58:24.987Z
