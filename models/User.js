fb21e18a5664965af20146ad5348f24b62e79491
Mitwa <parsanamitu@gmail.com>
2021-10-21 20:08:29 +0100

    Refactored API routes for better structure

--- models/User.js
+++ models/User.js
@@ -112,3 +112,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T21:42:30.220Z
 // Last updated: 2025-02-20T22:08:39.807Z
 // Last updated: 2025-02-20T22:08:41.626Z
+// Last updated: 2025-02-20T22:08:44.701Z
