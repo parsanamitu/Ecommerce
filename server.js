12228aaffb87c21639712e3a29e40aebd4bcccfc
Mitwa <parsanamitu@gmail.com>
2022-01-14 17:27:17 +0000

    Refactored API routes for better structure

--- server.js
+++ server.js
@@ -143,3 +143,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Last updated: 2025-02-20T22:09:08.137Z
 // Last updated: 2025-02-20T22:09:08.642Z
 // Last updated: 2025-02-20T22:09:10.145Z
+// Last updated: 2025-02-20T22:09:13.697Z
