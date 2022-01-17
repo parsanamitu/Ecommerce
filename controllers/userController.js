73973d3e35e79f018d9903ce8bbd393938f68aec
madhavi <madhavibhatt72@gmail.com>
2022-01-17 19:44:38 +0000

    Improved logging for error tracking

--- controllers/userController.js
+++ controllers/userController.js
@@ -68,3 +68,4 @@ export const getUsers = async (req, res) => {
 // Last updated: 2025-02-20T20:58:52.903Z
 // Last updated: 2025-02-20T20:58:53.814Z
 // Last updated: 2025-02-20T20:58:54.616Z
+// Last updated: 2025-02-20T20:58:55.625Z
