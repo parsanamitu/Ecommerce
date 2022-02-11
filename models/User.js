081b2b3500d5394a03b5cccbbd843f8a4e78c62d
madhavi <madhavibhatt72@gmail.com>
2022-02-11 22:00:46 +0000

    Fixed session expiration issue

--- models/User.js
+++ models/User.js
@@ -107,3 +107,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T21:42:26.505Z
 // Last updated: 2025-02-20T21:42:27.021Z
 // Last updated: 2025-02-20T21:42:28.316Z
+// Last updated: 2025-02-20T21:42:29.485Z
