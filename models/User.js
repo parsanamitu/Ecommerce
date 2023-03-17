0ac47bccf9190a6016f7b153f5dcdd8c176126f7
Mitwa <parsanamitu@gmail.com>
2023-03-17 23:03:14 +0000

    Optimized DB queries for faster performance

--- models/User.js
+++ models/User.js
@@ -234,3 +234,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T22:18:24.055Z
 // Last updated: 2025-02-20T22:18:24.581Z
 // Last updated: 2025-02-20T22:18:25.986Z
+// Last updated: 2025-02-20T22:18:28.433Z
