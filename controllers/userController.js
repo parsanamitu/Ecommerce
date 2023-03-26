f4595618f968660191b191154becbd4fc3aab384
Mitwa <parsanamitu@gmail.com>
2023-03-26 14:03:36 +0100

    Optimized DB queries for faster performance

--- controllers/userController.js
+++ controllers/userController.js
@@ -210,3 +210,4 @@ export const getUsers = async (req, res) => {
 // Last updated: 2025-02-20T22:17:49.991Z
 // Last updated: 2025-02-20T22:17:54.483Z
 // Last updated: 2025-02-20T22:17:57.032Z
+// Last updated: 2025-02-20T22:17:59.738Z
