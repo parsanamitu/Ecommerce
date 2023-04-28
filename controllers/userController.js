500928f36b02a61b69743c776d03f1f45d938b1d
madhavi <madhavibhatt72@gmail.com>
2023-04-28 12:00:02 +0100

    Implemented JWT authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,3 +14,4 @@ export const getUsers = async (req, res) => {
 // Last updated: 2025-02-20T20:42:18.933Z
 // Last updated: 2025-02-20T20:42:21.392Z
 // Last updated: 2025-02-20T20:42:23.262Z
+// Last updated: 2025-02-20T20:42:26.765Z
