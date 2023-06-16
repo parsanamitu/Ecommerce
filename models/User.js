d7722a60c61312bc6d20403b3da7ed000c9851b2
Mitwa <parsanamitu@gmail.com>
2023-06-16 20:29:15 +0100

    Added role-based access control

--- models/User.js
+++ models/User.js
@@ -215,3 +215,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T22:17:51.774Z
 // Last updated: 2025-02-20T22:17:52.998Z
 // Last updated: 2025-02-20T22:17:53.730Z
+// Last updated: 2025-02-20T22:17:54.724Z
