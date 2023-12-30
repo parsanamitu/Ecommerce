d4f37bc2875bd1fcd993a5ff7a2de4edab130f33
Mitwa <parsanamitu@gmail.com>
2023-12-30 09:01:09 +0000

    Fixed session expiration issue

--- models/User.js
+++ models/User.js
@@ -224,3 +224,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T22:18:05.736Z
 // Last updated: 2025-02-20T22:18:08.358Z
 // Last updated: 2025-02-20T22:18:09.340Z
+// Last updated: 2025-02-20T22:18:10.346Z
