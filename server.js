2b7cb55b185b86389374ced06dcc50363f5c01c4
Mitwa <parsanamitu@gmail.com>
2024-05-13 12:11:46 +0100

    Implemented JWT authentication

--- server.js
+++ server.js
@@ -263,3 +263,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Last updated: 2025-02-20T22:20:26.359Z
 // Last updated: 2025-02-20T22:20:26.919Z
 // Last updated: 2025-02-20T22:20:28.594Z
+// Last updated: 2025-02-20T22:20:33.149Z
