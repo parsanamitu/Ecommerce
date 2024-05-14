360196533af6ee3353ebf7eb12f66ab8fca6515d
Mitwa <parsanamitu@gmail.com>
2024-05-14 13:24:21 +0100

    Enhanced security by sanitizing user input

--- controllers/userController.js
+++ controllers/userController.js
@@ -188,3 +188,4 @@ export const getUsers = async (req, res) => {
 // Last updated: 2025-02-20T22:15:33.217Z
 // Last updated: 2025-02-20T22:15:33.707Z
 // Last updated: 2025-02-20T22:17:06.102Z
+// Last updated: 2025-02-20T22:17:08.433Z
