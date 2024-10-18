b11a4ed5b7beb704b4076c19fb3c4570b7b0d867
Mitwa <parsanamitu@gmail.com>
2024-10-18 12:02:02 +0100

    Added role-based access control

--- server.js
+++ server.js
@@ -230,3 +230,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Last updated: 2025-02-20T22:18:12.559Z
 // Last updated: 2025-02-20T22:18:13.085Z
 // Last updated: 2025-02-20T22:18:13.588Z
+// Last updated: 2025-02-20T22:18:14.800Z
