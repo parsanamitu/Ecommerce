c2891eab00294568d6f55544d985c6d22d4d23de
Mitwa <parsanamitu@gmail.com>
2024-10-25 12:06:41 +0100

    Enhanced security by sanitizing user input

--- models/User.js
+++ models/User.js
@@ -196,3 +196,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T22:17:27.191Z
 // Last updated: 2025-02-20T22:17:27.449Z
 // Last updated: 2025-02-20T22:17:29.197Z
+// Last updated: 2025-02-20T22:17:30.021Z
