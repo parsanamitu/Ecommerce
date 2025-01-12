965d21af240bbf6be92c3a97aec0f6237444329b
Mitwa <parsanamitu@gmail.com>
2025-01-12 09:49:07 +0000

    Improved logging for error tracking

--- models/User.js
+++ models/User.js
@@ -249,3 +249,4 @@ export default mongoose.model("User", UserSchema);
 // Last updated: 2025-02-20T22:18:42.550Z
 // Last updated: 2025-02-20T22:19:38.842Z
 // Last updated: 2025-02-20T22:19:45.179Z
+// Last updated: 2025-02-20T22:19:48.941Z
